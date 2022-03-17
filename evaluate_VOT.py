6fb8b09d2ec6c814375257cfb00c8669c8fe8615
magister <michaldanilow@gmail.com>
2022-03-17 17:56:11 +0100

    show_VOT_dataset mosaic option

--- evaluate_VOT.py
+++ evaluate_VOT.py
@@ -4,6 +4,7 @@ import cv2
 import argparse
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 from utils import bbox_iou, load_gt, init_seeds
 from mosse import mosse
@@ -13,30 +14,75 @@ from deep_mosse import DeepMosse
 # init_seeds()
 
 
-def show_VOT_dataset(dataset_path):
+def show_VOT_dataset(dataset_path, draw_trajectory=False, standard='vot2015', mosaic=False):
 
     sequences = os.listdir(dataset_path)
-    for seq in sequences:
+    sequences = [seq for seq in sequences if os.path.isdir(join(dataset_path, seq))]
+    sequences = ['david', 'iceskater', 'face', 'singer', 'bicycle', 'bolt']
+
+    imgs_per_seq = 6
+    mosaic_type = 'rowwise'
+    if mosaic_type == 'rowwise':
+        f, axarr = plt.subplots(len(sequences), imgs_per_seq)
+    else:
+        f, axarr = plt.subplots(imgs_per_seq, len(sequences))
+
+    for i_seq, seq in enumerate(sequences):
         seqpath = join(dataset_path, seq)
-        imgpath = join(seqpath, 'img')
-        gt = load_gt(join(seqpath, 'groundtruth.txt'))
+        imgpath = join(seqpath, 'color')
+        gt = load_gt(join(seqpath, 'groundtruth.txt'), standard=standard)
         imgnames = os.listdir(imgpath)
         imgnames.sort()
-        for imgname, bbox in zip(imgnames, gt):
-            img = cv2.imread(join(imgpath, imgname))
-            cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), (255, 0, 0), 2)
-            cv2.imshow(seq, img)
-            if cv2.waitKey(0) == ord('q'):
-                break
+        trajectory = []
+
+        if mosaic:
+            
+            img_indices = list(np.linspace(0, len(imgnames)-1, imgs_per_seq))
+            img_indices = [int(el) for el in img_indices]
+            print('indices:', img_indices)
+            print('lens:', len(gt), len(imgnames))
+            for i, img_ind in enumerate(img_indices):
+                bbox = gt[img_ind]
+                img = cv2.imread(join(imgpath, imgnames[img_ind]))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), (255, 0, 0), 2)
+                # cv2.imshow('dupa', img)
+                # if cv2.waitKey(0) == ord('q'):
+                #     break
+                if mosaic_type == 'rowwise':
+                    axarr[i_seq, i].imshow(img)
+                    axarr[i_seq, i].axis('off')
+                else:
+                    axarr[i, i_seq].imshow(img)
+                    axarr[i, i_seq].axis('off')
+        else:
+            for imgname, bbox in zip(imgnames, gt):
+                img = cv2.imread(join(imgpath, imgname))
+                if draw_trajectory:
+                    center_xy = (bbox[0]+bbox[2]//2, bbox[1]+bbox[3]//2)
+                    trajectory.append(center_xy)
+                    for i, center in enumerate(trajectory):
+                        if i >= 2:
+                            cv2.line(img, trajectory[i-1], trajectory[i], color=(0, 255, 255), thickness=2)
+                        # cv2.circle(img, center, radius=1, color=(0, 0, 255), thickness=-1)
+                cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), (255, 0, 0), 2)
+                cv2.imshow(seq, img)
+                if cv2.waitKey(0) == ord('q'):
+                    break
+
+    if mosaic:
+        plt.axis('off')
+        plt.show()
 
 
 def test_sequence(sequence):
 
     seqdir = join(DATASET_DIR, sequence)
-    imgdir = join(seqdir, 'img')
+    imgdir = join(seqdir, 'color')
     imgnames = os.listdir(imgdir)                  
     imgnames.sort()
 
+    print('xd:', join(imgdir, imgnames[0]))
     init_img = cv2.imread(join(imgdir, imgnames[0]))
     gt_boxes = load_gt(join(seqdir, 'groundtruth.txt'))
     tracker = DeepMosse(init_img, gt_boxes[0], args)
@@ -86,10 +132,13 @@ parse.add_argument('--quantized', action='store_true')
 args = parse.parse_args()
 print('args:', args)
 
-DATASET_DIR = '../datasets/VOT2013'
+# DATASET_DIR = '../datasets/VOT2013'
+DATASET_DIR = 'vot_workspace2013/sequences'
 CONV_NETWORK_NAME = 'yolo_finn_6conv_8w8a_160x320_5anchors'
 NET_CONFIG = join('networks', CONV_NETWORK_NAME, 'config.json')
 NET_WEIGHTS = join('networks', CONV_NETWORK_NAME, 'test_best.pt')
+
+show_VOT_dataset(DATASET_DIR, draw_trajectory=False, standard='vot2013', mosaic=True)
 
 if args.seq == 'all':
     sequences = os.listdir(DATASET_DIR)
