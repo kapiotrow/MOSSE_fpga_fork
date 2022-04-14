36d0e912b46eca97458dbeff0667b4c3be2b439d
magister <michaldanilow@gmail.com>
2022-04-14 19:17:35 +0200

    my accelerator

--- backbone_test.py
+++ backbone_test.py
@@ -1,6 +1,8 @@
 import torchvision.transforms as transforms
 import cv2
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 
 from imagenet.finn_models import get_finnlayer
 
@@ -17,13 +19,33 @@ def cnn_preprocess(data):
 
         return result
 
-
+pad = 2
 backbone = get_finnlayer('output/finnlayer_weights/savegame_0_15000.pth.tar', strict=False)
 backbone.to(torch.device('cuda:0'))
 test_img = cv2.imread('test_input.ppm')
+test_img = cv2.copyMakeBorder(test_img, pad, pad, pad, pad, cv2.BORDER_CONSTANT)
+
 # test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
 test_input = cnn_preprocess(test_img)
 test_output = backbone(test_input)
 
-print(test_img[:, :, 0])
-print(test_output[0, 0])
+
+# x = np.range()
+# print('x shape:', x.shape)
+# conv = torch.nn.Conv2d(3, 1, kernel_size=3, stride=3, bias=False)
+# conv.weight = torch.nn.Parameter(torch.ones((1, 1, 3, 3)))
+# # conv.bias = torch.nn.Parameter(torch.tensor(0.69).reshape((1)))
+# print(conv.weight)
+# pool = torch.nn.MaxPool2d((3, 3))
+# y = conv(torch.tensor(x).float())
+# print(y.shape)
+
+# print(test_img.shape)
+# np.savetxt('R.csv', test_img[:, :, 0].astype(np.uint8))
+# print(test_img[213:, :10, 0])
+# print(test_img[213:, :10, 1])
+# print(test_img[213:, :10, 2])
+# plt.imshow(test_img)
+# plt.show()
+# print(test_output[0, 0])
+# print('min:', test_output.min())
--- deployment/verilog_modules/hdmi_in.v
+++ deployment/verilog_modules/hdmi_in.v
@@ -5,45 +5,28 @@
 // Create Date: 23:14:48 04/19/2011 
 // Description: vga generator
 //-----------------------------------------------
-module hdmi_in
+module hdmi_in #(
+    parameter IMG_PATH = ""
+)
 (
   //hdmi outputs
   output reg hdmi_clk,
-  output hdmi_de,
-  output hdmi_hs,
-  output hdmi_vs,
-  //image data
-  output [7:0]hdmi_r,
-  output [7:0]hdmi_g,
-  output [7:0]hdmi_b
+  
+  //master axis interface
+  output reg [7:0] m_axis_0_tdata,
+  output reg m_axis_0_tvalid,
+  input m_axis_0_tready,
+  output reg [17:0] pixel_cnt = 0
 ); 
 //-----------------------------------------------
   parameter IGNORE_HEADER_CHARACTERS = 15;
-//for now supports VGA 640x480 60Hz only
-  //horizontal
-  parameter hr=224; //resolution
-  parameter hbp=8; //back porch
-  parameter hfp=8; //front porch
-  parameter hs=2;  //sync len
-  //vertical
-  parameter vr=224; //resolution
-  parameter vbp=8; //back porch
-  parameter vfp=8; //front porch
-  parameter vs=4;   //sync len
+  parameter height = 224; //resolution
+  parameter width = 224; //resolution
+  parameter total_pixels = height * width * 3;
 //-----------------------------------------------
-  reg line=1'b0;
-  reg vsync=1'b1;
-  reg hsync=1'b1;
-  reg enab=1'b0;
-  reg eenab=1'b0;
-  reg h_enable=1'b0;
-  reg v_enable=1'b0;
-  reg [10:0]hcounter=0;
-  reg [10:0]vcounter=64+1;//480+7
+  reg skip_header = 1;
+//  reg [17:0] pixel_cnt = 0;
 //-----------------------------------------------
-  reg [7:0]red;
-  reg [7:0]green;
-  reg [7:0]blue;
   
   //reg hdmi_clk=1'b0;
 //-----------------------------------------------
@@ -59,89 +42,80 @@ end
 integer rgbfile,i,v,clo,cle,wl,x;
 
 //-----------------------------------------------
-always @(posedge hdmi_clk)
-begin
-  hcounter<=hcounter+1;
+//always @(posedge hdmi_clk)
+//begin
+//  hcounter<=hcounter+1;
   
-  eenab<=enab;
+//  eenab<=enab;
 
-  if(hcounter==(hr+hbp)) begin
-    hsync<=1'b0;
-  end
-  if(hcounter==(hr+hbp+hs)) begin
-    hsync<=1'b1;
-	 line<=1'b0;
-  end
+//  if(hcounter==(hr+hbp)) begin
+//    hsync<=1'b0;
+//  end
+//  if(hcounter==(hr+hbp+hs)) begin
+//    hsync<=1'b1;
+//	 line<=1'b0;
+//  end
 
-  if(hcounter<hr) 
-      h_enable<=1'b1;
-  else 
-		h_enable<=1'b0;
+//  if(hcounter<hr) 
+//      h_enable<=1'b1;
+//  else 
+//		h_enable<=1'b0;
   
-  if(vcounter<vr) 
-		v_enable<=1'b1;
-  else 
-		v_enable<=1'b0;
+//  if(vcounter<vr) 
+//		v_enable<=1'b1;
+//  else 
+//		v_enable<=1'b0;
 		
-  if((v_enable==1'b1)&&(h_enable==1'b1))
-		enab<=1'b1;
-  else 
-		enab<=1'b0;
+//  if((v_enable==1'b1)&&(h_enable==1'b1))
+//		enab<=1'b1;
+//  else 
+//		enab<=1'b0;
 		  	  
-  if(hcounter==(hr+hbp+hs+hfp)) 
-  begin
-    hcounter<=0;
-	 line<=1'b1;
-  end
-end
+//  if(hcounter==(hr+hbp+hs+hfp)) 
+//  begin
+//    hcounter<=0;
+//	 line<=1'b1;
+//  end
+//end
 //-----------------------------------------------
 //TB only
 
 always @(posedge hdmi_clk)
 begin
-  if(enab)
-  begin
-    red=$fgetc(rgbfile);
-    green=$fgetc(rgbfile);
-    blue=$fgetc(rgbfile);
- 	 
-  end
-end
-//TB end
-//-----------------------------------------------
-reg [7:0]vsc=0;
-
-always @(posedge line)
-begin
-  vcounter<=vcounter+1;
-  if(vcounter==(vr+vbp))
-  begin  
-    vsync<=1'b0;
-	 //TB only
-	 vsc=vsc+1;
-
-    rgbfile = $fopen("/home/magister/CF_tracking/MOSSE_fpga/test_input.ppm","rb");
-
-	 // read header file
-	 for(i=0; i<IGNORE_HEADER_CHARACTERS; i=i+1)
+    if(skip_header == 0 && m_axis_0_tready)
+    begin
+        m_axis_0_tdata <= $fgetc(rgbfile);
+        m_axis_0_tvalid <= 1;
+        pixel_cnt <= pixel_cnt + 1;
+        
+        if(pixel_cnt == total_pixels - 1)
         begin
-            v=$fgetc(rgbfile); 
-        end	
-	 //TB end
-  end
-  if(vcounter==(vr+vbp+vs)) vsync<=1'b1;
-  if(vcounter==(vr+vbp+vs+vfp)) vcounter<=0;
+            pixel_cnt <= 0;
+            skip_header <= 1;
+        end
+    end
+    
+    else
+    begin
+        m_axis_0_tvalid <= 0;
+    end
 end
-//-----------------------------------------------
 
-assign hdmi_r=red;
-assign hdmi_g=green;
-assign hdmi_b=blue;
 
-assign hdmi_de=eenab;
-// +TK Changed to negatve for Zybo
-assign hdmi_hs=!hsync;
-assign hdmi_vs=!vsync;
-//-----------------------------------------------
+always @(posedge hdmi_clk)
+begin
+    if(skip_header == 1)
+    begin  
+        rgbfile = $fopen(IMG_PATH,"rb");
+        
+        // read header file
+        for(i=0; i<IGNORE_HEADER_CHARACTERS; i=i+1)
+        begin
+            $fgetc(rgbfile); 
+        end	
+        skip_header <= 0;
+    end
+end
+
 endmodule
-//-----------------------------------------------
+
